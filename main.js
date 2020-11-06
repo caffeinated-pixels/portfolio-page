0c045879bcb233d653fb937940e7a7726b000580
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2020-11-06 13:49:11 -0500

    Initial Commit

---
+++ main.js
@@ -0,0 +1,8 @@
+$(function() { //start document ready function
+  $(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top + -75
+    }, 1000);
+  }); // offsets anchor by 75px (size of the header) and adds in scroll animation
+}); //end of document ready
