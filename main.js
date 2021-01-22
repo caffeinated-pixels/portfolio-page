6bea5da1f79fdf5c535356d67cab75e0d2ec48f0
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-01-22 09:55:01 -0500

    activated jQ logic

--- main.js
+++ main.js
@@ -11,19 +11,19 @@ $(function() {
   }) // offsets anchor by 75px (size of the header) and adds in scroll animation
 
   // click hander for burger btn and nav-links on smaller screens; toggles dropdown menu
-  // $('.burger-btn, .nav-link').on('click', event => {
-  //   event.preventDefault()
-  //
-  //   // check window size so only executed on smaller screens
-  //   if ($(window).width() <= 1024) {
-  //     $('.nav-list').slideToggle(250)
-  //   }
-  // })
-  //
-  // // the click handler above adds a display style attr to .nav-list, which can cause problems when enlarging the window (i.e. the menu items dissappear!). This event listener removes that attr when window > 1024px
-  // $(window).resize(() => {
-  //   if ($(window).width() > 1024) {
-  //     $('.nav-list').removeAttr('style')
-  //   }
-  // })
+  $('.burger-btn, .nav-link').on('click', event => {
+    event.preventDefault()
+
+    // check window size so only executed on smaller screens
+    if ($(window).width() <= 768) {
+      $('.nav-list').slideToggle(250)
+    }
+  })
+
+  // the click handler above adds a display style attr to .nav-list, which can cause problems when enlarging the window (i.e. the menu items dissappear!). This event listener removes that attr when window > 1024px
+  $(window).resize(() => {
+    if ($(window).width() > 768) {
+      $('.nav-list').removeAttr('style')
+    }
+  })
 }) //end of document ready
