f79585c7950701bfc045acaba99693c2826b47b7
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-01-19 06:39:35 -0500

    Add main tech to project names

--- main.js
+++ main.js
@@ -1,102 +1,102 @@
 const projects = [
   {
-    name: 'Weather App',
+    name: 'Weather App (React)',
     website: 'https://caffeinated-weather.netlify.app/',
     image: './images/caffeinated-weather.jpg',
-    altText: 'Weather app screen grab'
+    altText: 'Weather app screen grab',
   },
   {
-    name: 'Pac-man clone',
+    name: 'Pac-man clone (JS)',
     website: 'https://mercboy-pacman.netlify.app/',
     image: './images/pacman.png',
-    altText: 'Pacman game screen grab'
+    altText: 'Pacman game screen grab',
   },
   {
-    name: 'Snake Game',
+    name: 'Snake Game (JS)',
     website: 'https://mercboy-snake.netlify.app/',
     image: './images/mercboy-snake.png',
-    altText: 'snake game screen grab'
+    altText: 'snake game screen grab',
   },
   {
-    name: 'Pomodoro (25+5) Clock',
+    name: 'Pomodoro (25+5) Clock (React)',
     website: 'https://caffeinated-pomodoro-clock.netlify.app/',
     image: './images/pomodoro-clock.jpg',
-    altText: 'Pomodoro (25+5) Clock screen grab'
+    altText: 'Pomodoro (25+5) Clock screen grab',
   },
   {
-    name: 'JavaScript calculator',
+    name: 'JavaScript calculator (React)',
     website: 'https://mercenary-calculator.netlify.app/',
     image: './images/javascript-calculator.jpg',
-    altText: 'JavaScript calculator screen grab'
+    altText: 'JavaScript calculator screen grab',
   },
   {
-    name: 'To-do app',
+    name: 'To-do app (React)',
     website: 'https://caffeinated-todo-app.netlify.app/',
     image: './images/to-do-app.png',
-    altText: 'To-do app screen grab'
+    altText: 'To-do app screen grab',
   },
   {
-    name: 'Drum Machine',
+    name: 'Drum Machine (React)',
     website: 'https://merc-drum-machine.netlify.app/',
     image: './images/drum-machine.jpg',
-    altText: 'Drum Machine screen grab'
+    altText: 'Drum Machine screen grab',
   },
   {
-    name: 'Markdown Previewer',
+    name: 'Markdown Previewer (React)',
     website: 'https://markup-my-markdown.netlify.app/',
     image: './images/markdown-previewer.jpg',
-    altText: 'Markdown Previewer screen grab'
+    altText: 'Markdown Previewer screen grab',
   },
   {
-    name: 'Random Quote Machine',
+    name: 'Random Quote Machine (jQuery)',
     website: 'https://quote-o-matic-4000.netlify.app/',
     image: './images/random-quote-machine.jpg',
-    altText: 'Random Quote Machine screen grab'
+    altText: 'Random Quote Machine screen grab',
   },
   {
-    name: 'Technical Documentation Page',
+    name: 'Technical Documentation Page (HTML/CSS)',
     website: 'https://cssflex-tech-doc.netlify.app/',
     image: './images/technical-documentation-page.jpg',
-    altText: 'Technical Documentation Page screen grab'
+    altText: 'Technical Documentation Page screen grab',
   },
   {
-    name: 'Product Landing Page',
+    name: 'Product Landing Page (HTML/CSS)',
     website: 'https://moonsorrow-industries.netlify.app/',
     image: './images/product-landing-page.jpg',
-    altText: 'Product landing page project screen grab'
+    altText: 'Product landing page project screen grab',
   },
   {
-    name: 'Survey Form',
+    name: 'Survey Form (HTML/CSS)',
     website: 'https://caffeinatedpixelsurveyform.netlify.app/',
     image: './images/survey-form.jpg',
-    altText: 'Survey Form project screen grab'
+    altText: 'Survey Form project screen grab',
   },
   {
-    name: 'Tetris',
+    name: 'Tetris (JS)',
     website: 'https://caffeinated-tetris.netlify.app/',
     image: './images/basic-tetris.jpg',
-    altText: 'Tetris game screen grab'
+    altText: 'Tetris game screen grab',
   },
   {
-    name: 'Wikipedia Viewer',
+    name: 'Wikipedia Viewer (jQuery)',
     website: 'https://codepen.io/cakeisaliegaming/full/aVXKpN',
     image: './images/wikipedia-viewer.jpg',
-    altText: 'Wikipedia Viewer project screen grab'
+    altText: 'Wikipedia Viewer project screen grab',
   },
   {
-    name: 'Tribute Page',
+    name: 'Tribute Page (HTML/CSS)',
     website: 'https://roquefort-tribute.netlify.app/',
     image: './images/tribute-page-roquefort.jpg',
-    altText: 'Tribute Page project screen grab'
-  }
+    altText: 'Tribute Page project screen grab',
+  },
 ]
 
-$(function() {
+$(function () {
   //start document ready function
 
   let projectGridHTML = ''
 
-  projects.forEach(project => {
+  projects.forEach((project) => {
     const html = `
       <div class="project-tile">
         <a href="${project.website}">
@@ -114,18 +114,18 @@ $(function() {
 
   $('#project-grid').html(projectGridHTML)
 
-  $(document).on('click', 'a[href^="#"]', function(event) {
+  $(document).on('click', 'a[href^="#"]', function (event) {
     event.preventDefault()
     $('html, body').animate(
       {
-        scrollTop: $($.attr(this, 'href')).offset().top + -75
+        scrollTop: $($.attr(this, 'href')).offset().top + -75,
       },
       1000
     )
   }) // offsets anchor by 75px (size of the header) and adds in scroll animation
 
   // click hander for burger btn and nav-links on smaller screens; toggles dropdown menu
-  $('.burger-btn, .nav-link').on('click', event => {
+  $('.burger-btn, .nav-link').on('click', (event) => {
     event.preventDefault()
 
     // check window size so only executed on smaller screens
