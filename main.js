55fb9745bf7d592245233128e308cc1611948021
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2025-07-08 09:12:19 -0400

    feat: add Parcel configuration and refactor image imports in main.js
    - Introduce .parcelrc to extend default Parcel configuration and add support for WEBP image transformation.
    - Refactor image imports in main.js to use imported variables instead of direct paths for better maintainability.

--- main.js
+++ main.js
@@ -1,100 +1,116 @@
 import $ from 'jquery'
+import retrofixImg from '/images/retrofix.webp'
+import weatherImg from '/images/caffeinated-weather.webp'
+import pacmanImg from '/images/pacman.webp'
+import tetrisImg from '/images/basic-tetris.webp'
+import snakeImg from '/images/mercboy-snake.webp'
+import pomodoroImg from '/images/pomodoro-clock.webp'
+import calculatorImg from '/images/javascript-calculator.webp'
+import todoImg from '/images/to-do-app.webp'
+import drumMachineImg from './images/drum-machine.webp'
+import markdownImg from './images/markdown-previewer.webp'
+import quoteMachineImg from './images/random-quote-machine.webp'
+import techDocImg from './images/technical-documentation-page.webp'
+import landingPageImg from './images/product-landing-page.webp'
+import surveyFormImg from './images/survey-form.webp'
+import wikiViewerImg from './images/wikipedia-viewer.webp'
+import tributePageImg from './images/tribute-page-roquefort.webp'
 
 const projects = [
   {
     name: 'NetFlix clone (Next.js/TS)',
     website: 'https://retrofix-nextjs.netlify.app',
-    image: '/images/retrofix.webp',
+    image: retrofixImg,
     altText: 'Netflix clone screen grab',
   },
   {
     name: 'Weather App (React/TS)',
     website: 'https://caffeinated-weather.netlify.app/',
-    image: '/images/caffeinated-weather.webp',
+    image: weatherImg,
     altText: 'Weather app screen grab',
   },
   {
     name: 'Pac-man clone (TS)',
     website: 'https://mercboy-pacman.netlify.app/',
-    image: '/images/pacman.webp',
+    image: pacmanImg,
     altText: 'Pacman game screen grab',
   },
   {
     name: 'Tetris (TS)',
     website: 'https://caffeinated-tetris.netlify.app/',
-    image: '/images/basic-tetris.webp',
+    image: tetrisImg,
     altText: 'Tetris game screen grab',
   },
   {
     name: 'Snake Game (JS)',
     website: 'https://mercboy-snake.netlify.app/',
-    image: '/images/mercboy-snake.webp',
+    image: snakeImg,
     altText: 'snake game screen grab',
   },
   {
     name: 'Pomodoro (25+5) Clock (React)',
     website: 'https://caffeinated-pomodoro-clock.netlify.app/',
-    image: '/images/pomodoro-clock.webp',
+    image: pomodoroImg,
     altText: 'Pomodoro (25+5) Clock screen grab',
   },
   {
     name: 'JavaScript calculator (React/TS)',
     website: 'https://mercenary-calculator.netlify.app/',
-    image: '/images/javascript-calculator.webp',
+    image: calculatorImg,
     altText: 'JavaScript calculator screen grab',
   },
   {
     name: 'To-do app (React/TS)',
     website: 'https://caffeinated-todo-app.netlify.app/',
-    image: '/images/to-do-app.webp',
+    image: todoImg,
     altText: 'To-do app screen grab',
   },
   {
     name: 'Drum Machine (React)',
     website: 'https://merc-drum-machine.netlify.app/',
-    image: '/images/drum-machine.webp',
+    image: drumMachineImg,
     altText: 'Drum Machine screen grab',
   },
   {
     name: 'Markdown Previewer (React)',
     website: 'https://markup-my-markdown.netlify.app/',
-    image: '/images/markdown-previewer.webp',
+    image: markdownImg,
     altText: 'Markdown Previewer screen grab',
   },
   {
     name: 'Random Quote Machine (jQuery)',
     website: 'https://quote-o-matic-4000.netlify.app/',
-    image: '/images/random-quote-machine.webp',
+    image: quoteMachineImg,
     altText: 'Random Quote Machine screen grab',
   },
   {
     name: 'Technical Documentation Page (HTML/CSS)',
     website: 'https://cssflex-tech-doc.netlify.app/',
-    image: '/images/technical-documentation-page.webp',
+    image: techDocImg,
     altText: 'Technical Documentation Page screen grab',
   },
   {
     name: 'Product Landing Page (HTML/CSS)',
     website: 'https://moonsorrow-industries.netlify.app/',
-    image: '/images/product-landing-page.webp',
+    image: landingPageImg,
     altText: 'Product landing page project screen grab',
   },
   {
     name: 'Survey Form (HTML/CSS)',
     website: 'https://caffeinatedpixelsurveyform.netlify.app/',
-    image: '/images/survey-form.webp',
+    image: surveyFormImg,
     altText: 'Survey Form project screen grab',
   },
   {
     name: 'Wikipedia Viewer (jQuery)',
     website: 'https://codepen.io/cakeisaliegaming/full/aVXKpN',
-    image: '/images/wikipedia-viewer.webp',
+    image: wikiViewerImg,
     altText: 'Wikipedia Viewer project screen grab',
   },
   {
     name: 'Tribute Page (HTML/CSS)',
     website: 'https://roquefort-tribute.netlify.app/',
-    image: '/images/tribute-page-roquefort.webp',
+    image: tributePageImg,
     altText: 'Tribute Page project screen grab',
   },
 ]
